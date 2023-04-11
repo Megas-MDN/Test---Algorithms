cdaed560ab6be1a41b659bc76582583aa320f482
Megas-MDN <mdn.melqui@gmail.com>
2023-04-11 15:39:53 -0300

    feat: :zap: One dimension array

---
+++ 01-one-dimensional-array.js
@@ -0,0 +1,18 @@
+const getOnlyType = (arr, type) => arr.filter((el) => typeof el === type);
+
+const greaterThan = (arr) => [...arr].sort((a, b) => b - a)[0];
+
+const main = (arr) => {
+  if (!Array.isArray(arr))
+    throw new Error('Sorry, only array type is allowed!');
+
+  const onlyNumbers = getOnlyType(arr, 'number');
+  const onlyStrings = getOnlyType(arr, 'string');
+  const highestNumber = greaterThan(onlyNumbers);
+
+  console.log(onlyNumbers, onlyStrings, highestNumber);
+
+  return [onlyNumbers, onlyStrings, highestNumber];
+};
+
+module.exports = main;
